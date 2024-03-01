a3dae5c7ca1b13d58eaf4f74fedf943b67d1fed3
Araceli Fernández <126494947+Araf05@users.noreply.github.com>
2024-03-01 16:00:27 -0300

    Crea matriz con llaves de encriptación

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-let textoInput;
+let texto_Input, texto_Output;
+
+const matriz_codigo = [
+   ["a", "ai"],
+   ["e", "enter"],
+   ["i", "imes"],
+   ["o", "ober"],
+   ["u", "ufat"],
+];
 
 function encriptar(){
-   textoInput = document.getElementById('texto-ingresado').value;
-   console.log(textoInput);
+   texto_Input = document.getElementById('texto-ingresado').value;
+   console.log(texto_Input);
 }
+
+function desencriptar(){}
+
